d0a16a1395c3805036f51572441fbd420211576a
dell <dell@dell.com>
2022-02-18 00:02:10 +0000

    adding script to test post request

---
+++ testing_post.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+response = requests.post(
+    "https://warm-scrubland-07453.herokuapp.com/",
+    json={
+        "age": 52,
+        "workclass": "Self-emp-inc",
+        "fnlgt": 287927,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Wife",
+        "race": "White",
+        "sex": "Female",
+        "capital-gain": 15024,
+        "capital-loss": 0,
+        "hours-per-week": 40,
+        "native-country": "United-States"
+    },
+)
+
+print(response.json())
+ 
