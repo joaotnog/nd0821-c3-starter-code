0a4cfd8cf4f3f7869a4cbec3c20fe46d2f6222aa
dell <dell@dell.com>
2022-02-21 21:04:12 +0000

    adding performance metrics on test set

--- starter/train_model.py
+++ starter/train_model.py
@@ -7,7 +7,7 @@ import pandas as pd
 import joblib
 
 # Add the necessary imports for the starter code.
-data = pd.read_csv('starter/data_mod/census_mod.csv')
+data = pd.read_csv('./data_mod/census_mod.csv')
 
 # Add code to load in the data.
 
@@ -29,12 +29,22 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=True
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, 
+    encoder = encoder, lb = lb, label="salary", training=False
 )
+
 
 # Train and save a model.
 model = train_model(X_train, y_train)
 joblib.dump(model, 'starter/model/model.pkl')
 joblib.dump(encoder, 'starter/model/encoder.pkl')
 joblib.dump(lb, 'starter/model/lb.pkl')
+
+# Compute metrics on test 
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+dict_test_metrics = dict(precision = precision,
+                         recall = recall,
+                         fbeta = fbeta)
+joblib.dump(dict_test_metrics, './model/dict_test_metrics.pkl')
